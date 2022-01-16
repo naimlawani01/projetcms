20f5676437998e00cec266c8cd6cc43c80100389
Naim Imrane Lawani <naimlawani01@gmail.com>
2022-01-16 15:56:03 +0100

    Nav responsive

--- wp-content/themes/twentytwentyone-child/taxonomy-genre.php
+++ wp-content/themes/twentytwentyone-child/taxonomy-genre.php
@@ -1,17 +1,15 @@
 <?php get_header(); ?>
 <?php $term = get_queried_object()?>
-    <nav class="navbar navbar-expand-sm navbar-light "  style="border-bottom:1px solid black; background-color: #FCFFF7;">
-		<div class="collapse navbar-collapse" id="collapsibleNavId">
-			<ul class="navbar-nav mx-auto mr-auto mt-2 mt-lg-0">
-				<li class="nav-item">
-					<a class="nav-link active" href="<?php echo get_post_type_archive_link('chaussure').'?genre='.$term->slug; ?>">Chaussures </a>
-				</li>
-				<li class="nav-item">
-					<a class="nav-link active" href="<?php echo get_post_type_archive_link('vetement').'?genre='.$term->slug; ?>">Vêtement</a>
-				</li>
-			</ul>
-		</div> 
-	</nav>
+
+    <ul class="nav mx-auto mr-auto mt-2 mt-lg-0 justify-content-center">
+        <li class="nav-item">
+            <a class="nav-link active text-reset" href="<?php echo get_post_type_archive_link('chaussure').'?genre='.$term->slug; ?>">Chaussures </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active text-reset" href="<?php echo get_post_type_archive_link('vetement').'?genre='.$term->slug; ?>">Vêtement</a>
+        </li>
+    </ul>
+
     <div class="card mb-3 container" >
         <div class="row g-0">
             <div class="col-md-4">
